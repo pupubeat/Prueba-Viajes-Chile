bbcf0a2c1341544b20c28a33f28c1dc30a3c12a7
pupubeat <ratenas95@gmail.com>
2024-02-17 10:07:28 -0300

    2do commit JShover en cards

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,3 +1,9 @@
 //Tooltip//
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Ad Hover fadeOut&fadeIn en Bootstrap Cards
+$(".card").hover(function () {
+    $(this).fadeOut(100);
+    $(this).fadeIn(500);
+});
