445f8c23f888228e5d6f6db5ed932f2c3b0cf479
pupubeat <ratenas95@gmail.com>
2024-02-22 17:39:40 -0300

    commit agregar document ready

--- assets/js/log.js
+++ assets/js/log.js
@@ -1,14 +1,18 @@
-// Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+$(document).ready(function () {
 
-// AdHover fadeOut&fadeIn en Bootstrap Cards
-$(".card").hover(function () {
-    $(this).fadeOut(100);
-    $(this).fadeIn(250);
-});
+    // Tooltip
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// Alert en Button de formulario
-$("#mailme").click(function () {
-    alert("¡Tu mensaje ha sido envíado!")
+    // AdHover fadeOut&fadeIn en Bootstrap Cards
+    $(".card").hover(function () {
+        $(this).fadeOut(100);
+        $(this).fadeIn(250);
+    });
+
+    // Alert en Button de formulario
+    $("#mailme").click(function () {
+        alert("¡Tu mensaje ha sido envíado!")
+    })
+
 })
